00a3e4113523bb8ca4f2e9d2f12d38f5faed223c
fbaldhagen <baldhagen@gmail.com>
2024-05-06 15:19:53 +0200

    Problem 2487

---
+++ LeetCodeSolutions/2401-2500/Problems_2481_2490.cs
@@ -0,0 +1,48 @@
+﻿using LeetCodeSolutions.Structures;
+
+namespace LeetCodeSolutions._2401_2500;
+
+public class Problems_2481_2490
+{
+    /// <summary>
+    /// Problem 2487
+    /// </summary>
+    /// <param name="head"></param>
+    /// <returns></returns>
+    public static ListNode RemoveNodes(ListNode head)
+    {
+        head = Reverse(head);
+        int max = head.val;
+        ListNode cur = head;
+
+        while (cur.next != null)
+        {
+            if (max > cur.next.val)
+            {
+                cur.next = cur.next.next;
+            }
+            else
+            {
+                max = cur.next.val;
+                cur = cur.next;
+            }
+        }
+
+        return Reverse(head);
+
+        static ListNode Reverse(ListNode cur)
+        {
+            ListNode prv = null;
+            ListNode next = cur;
+
+            while (next != null)
+            {
+                cur = next;
+                next = cur.next;
+                cur.next = prv;
+                prv = cur;
+            }
+            return cur;
+        }
+    }
+}
