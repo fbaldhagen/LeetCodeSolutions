5cf787c3364ebbaeee868fc8a0b2848c0f9abecd
fbaldhagen <baldhagen@gmail.com>
2024-05-16 22:06:34 +0200

    Problem 2331

---
+++ LeetCodeSolutions/2301-2400/Problems_2331_2340.cs
@@ -0,0 +1,44 @@
+﻿using LeetCodeSolutions.Structures;
+
+namespace LeetCodeSolutions._2301_2400;
+
+public class Problems_2331_2340
+{
+    /// <summary>
+    /// Problem 2331
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static bool EvaluateTree(TreeNode root)
+    {
+        // Helper function to evaluate the tree
+        static bool Evaluate(TreeNode node)
+        {
+            // If the node is a leaf, return its boolean value
+            if (node.left == null && node.right == null)
+            {
+                return node.val == 1;
+            }
+
+            // Recursively evaluate the left and right subtrees
+            bool leftValue = Evaluate(node.left);
+            bool rightValue = Evaluate(node.right);
+
+            // Apply the operation based on the node's value
+            if (node.val == 2) // OR operation
+            {
+                return leftValue || rightValue;
+            }
+            else if (node.val == 3) // AND operation
+            {
+                return leftValue && rightValue;
+            }
+
+            throw new InvalidOperationException("Invalid node value");
+        }
+
+        // Start evaluation from the root node
+        return Evaluate(root);
+    }
+}
