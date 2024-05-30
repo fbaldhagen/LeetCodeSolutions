a14a334a553eb2677215b3f0ee7e99341808bec1
fbaldhagen <baldhagen@gmail.com>
2024-05-30 16:17:23 +0200

    Problem 1442

---
+++ LeetCodeSolutions/1401-1500/Problems_1441_1450.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeSolutions._1401_1500;
+
+public class Problems_1441_1450
+{
+    /// <summary>
+    /// Problem 1442
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public static int CountTriplets(int[] arr)
+    {
+        int count = 0;
+
+        // Iterate over the starting point of the triplet (i)
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            // Initialize xor with the first element in the current subarray
+            int xor = arr[i];
+
+            // Iterate over the ending point of the triplet (k)
+            for (int k = i + 1; k < arr.Length; k++)
+            {
+                // Compute the cumulative xor from arr[i] to arr[k]
+                xor ^= arr[k];
+
+                // If xor is 0, we found a valid triplet
+                if (xor == 0)
+                {
+                    // All j (i < j <= k) form valid triplets with i and k
+                    count += k - i;
+                }
+            }
+        }
+
+        return count;
+    }
+}
