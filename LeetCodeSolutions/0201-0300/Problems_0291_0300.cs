6dc962f46f2a58cbec23d0e454f0fba62fdf3e64
fbaldhagen <baldhagen@gmail.com>
2024-05-31 17:36:40 +0200

    Problem 292

---
+++ LeetCodeSolutions/0201-0300/Problems_0291_0300.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeSolutions._0201_0300;
+
+public class Problems_0291_0300
+{
+    /// <summary>
+    /// Problem 292
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static bool CanWinNim(int n)
+    {
+        // If n is not a multiple of 4, you can win the game.
+        // If n is a multiple of 4, you will lose if both players play optimally.
+        return n % 4 != 0;
+    }
+
+}
