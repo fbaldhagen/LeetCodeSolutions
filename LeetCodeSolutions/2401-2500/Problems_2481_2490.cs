961b344ccd43cb035989d842cdb436e18146ce4f
fbaldhagen <baldhagen@gmail.com>
2024-06-03 23:58:03 +0200

    Problem 2486

--- LeetCodeSolutions/2401-2500/Problems_2481_2490.cs
+++ LeetCodeSolutions/2401-2500/Problems_2481_2490.cs
@@ -5,6 +5,31 @@ namespace LeetCodeSolutions._2401_2500;
 public class Problems_2481_2490
 {
     /// <summary>
+    /// Problem 2486
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static int AppendCharacters(string s, string t)
+    {
+        int sIndex = 0;
+        int tIndex = 0;
+
+        // Traverse through both strings
+        while (sIndex < s.Length && tIndex < t.Length)
+        {
+            if (s[sIndex] == t[tIndex])
+            {
+                tIndex++;
+            }
+            sIndex++;
+        }
+
+        // Characters remaining in t after the loop are the ones that need to be appended
+        return t.Length - tIndex;
+    }
+
+    /// <summary>
     /// Problem 2487
     /// </summary>
     /// <param name="head"></param>
