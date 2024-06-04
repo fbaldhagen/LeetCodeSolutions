a8ac92e315b52d359683224ae4fee4267adc09b2
fbaldhagen <baldhagen@gmail.com>
2024-06-04 12:30:39 +0200

    Problem 409

---
+++ LeetCodeSolutions/0401-0500/Problems_0401_0410.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCodeSolutions._0401_0500;
+
+public class Problems_0401_0410
+{
+    /// <summary>
+    /// Problem 409
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static int LongestPalindrome(string s)
+    {
+        Dictionary<char, int> map = [];
+
+        foreach (char c in s)
+        {
+            if (map.TryGetValue(c, out int value))
+            {
+                map[c] = ++value;
+            }
+            else
+            {
+                map.Add(c, 1);
+            }
+        }
+
+        int length = 0;
+        bool oddFound = false;
+
+        foreach (var value in map)
+        {
+            if (value.Value % 2 == 0)
+            {
+                length += value.Value;
+            }
+            else
+            {
+                length += value.Value - 1;
+                oddFound = true;
+            }
+        }
+
+        if (oddFound)
+        {
+            length++;
+        }
+
+        return length;
+    }
+}
