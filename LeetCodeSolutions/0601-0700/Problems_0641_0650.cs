ed8947f38b4c08a170b33f35bc395f79960228f9
fbaldhagen <baldhagen@gmail.com>
2024-06-07 03:26:02 +0200

    Problem 648

---
+++ LeetCodeSolutions/0601-0700/Problems_0641_0650.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeSolutions._0601_0700;
+
+public class Problems_0641_0650
+{
+    /// <summary>
+    /// Problem 648
+    /// </summary>
+    /// <param name="dictionary"></param>
+    /// <param name="sentence"></param>
+    /// <returns></returns>
+    public static string ReplaceWords(IList<string> dictionary, string sentence)
+    {
+        HashSet<string> set = new(dictionary);
+
+        string[] words = sentence.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            int wordLength = words[i].Length;
+
+            for (int j = 1; j < wordLength; j++)
+            {
+                if (set.Contains(words[i][..j]))
+                {
+                    words[i] = words[i][..j];
+                    break;
+                }
+            }
+        }
+
+        return string.Join(" ", words);
+    }
+}
