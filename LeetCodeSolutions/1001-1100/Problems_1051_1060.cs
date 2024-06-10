9478f59f4331fdda41b4edfe34ab26161a2e62d5
fbaldhagen <baldhagen@gmail.com>
2024-06-10 14:19:32 +0200

    Problem 1051

---
+++ LeetCodeSolutions/1001-1100/Problems_1051_1060.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeSolutions._1001_1100;
+
+public class Problems_1051_1060
+{
+    /// <summary>
+    /// Problem 1051
+    /// </summary>
+    /// <param name="heights"></param>
+    /// <returns></returns>
+    public static int HeightChecker(int[] heights)
+    {
+        int result = 0;
+
+        // Copy heights and sort the new array. 
+        int[] expected = new int[heights.Length];
+        Array.Copy(heights, expected, heights.Length);
+        Array.Sort(expected);
+        
+        // Check for mismatches, increment result if mismatched.
+        for (int i = 0; i < expected.Length; i++)
+        {
+            if (heights[i] != expected[i])
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
