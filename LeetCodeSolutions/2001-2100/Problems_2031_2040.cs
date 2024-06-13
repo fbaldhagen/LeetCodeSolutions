8e2ef2d659b068fcd47b0c40c9e7e4e605b1eef2
fbaldhagen <baldhagen@gmail.com>
2024-06-13 14:11:25 +0200

    Problem 2037

---
+++ LeetCodeSolutions/2001-2100/Problems_2031_2040.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeSolutions._2001_2100;
+
+public class Problems_2031_2040
+{
+    /// <summary>
+    /// Problem 2037
+    /// </summary>
+    /// <param name="seats"></param>
+    /// <param name="students"></param>
+    /// <returns></returns>
+    public static int MinMovesToSeat(int[] seats, int[] students)
+    {
+        Array.Sort(seats);
+        Array.Sort(students);
+
+        int diff = 0;
+        for (int i = 0; i < seats.Length; i++)
+        {
+            diff += Math.Abs(students[i] - seats[i]);
+        }
+
+        return diff;
+    }
+}
