10e8530432472c40f551c3eb91463ded6a01a766
fbaldhagen <baldhagen@gmail.com>
2024-06-15 23:08:52 +0200

    Problem 502

--- LeetCodeSolutions/0501-0600/Problems_0501_0510.cs
+++ LeetCodeSolutions/0501-0600/Problems_0501_0510.cs
@@ -5,6 +5,54 @@ namespace LeetCodeSolutions._0501_0600;
 public class Problems_0501_0510
 {
     /// <summary>
+    /// Problem 502
+    /// </summary>
+    /// <param name="k"></param>
+    /// <param name="w"></param>
+    /// <param name="profits"></param>
+    /// <param name="capital"></param>
+    /// <returns></returns>
+    public static int FindMaximizedCapital(int k, int w, int[] profits, int[] capital)
+    {
+        int n = profits.Length;
+
+        // Min-heap to store projects by their capital requirement
+        var minHeap = new PriorityQueue<(int capital, int profit), int>();
+
+        // Max-heap to store available projects by their profits
+        var maxHeap = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+
+        // Insert all projects into the min-heap
+        for (int i = 0; i < n; i++)
+        {
+            minHeap.Enqueue((capital[i], profits[i]), capital[i]);
+        }
+
+        int currentCapital = w;
+
+        for (int i = 0; i < k; i++)
+        {
+            // Move projects that can be unlocked with the current capital to the max-heap
+            while (minHeap.Count > 0 && minHeap.Peek().capital <= currentCapital)
+            {
+                var project = minHeap.Dequeue();
+                maxHeap.Enqueue(project.profit, project.profit);
+            }
+
+            // If no projects can be started, break the loop
+            if (maxHeap.Count == 0)
+            {
+                break;
+            }
+
+            // Select the project with the maximum profit
+            currentCapital += maxHeap.Dequeue();
+        }
+
+        return currentCapital;
+    }
+
+    /// <summary>
     /// Problem 506
     /// </summary>
     /// <param name="score"></param>
