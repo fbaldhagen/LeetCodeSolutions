90364039c2c5a18c4b90b59d266b200ea6a8dcc9
fbaldhagen <baldhagen@gmail.com>
2024-06-17 14:31:48 +0200

    Problem 633

---
+++ LeetCodeSolutions/0601-0700/Problems_0631_0640.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCodeSolutions._0601_0700;
+
+public class Problems_0631_0640
+{
+    /// <summary>
+    /// Problem 633
+    /// </summary>
+    /// <param name="c"></param>
+    /// <returns></returns>
+    public static bool JudgeSquareSum(int c)
+    {
+        long left = 0;
+        long right = (long)Math.Sqrt(c);
+
+        while (left <= right)
+        {
+            long sum = left * left + right * right;
+
+            if (sum == c)
+            {
+                return true;
+            }
+            else if (sum < c)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return false;
+    }
+}
