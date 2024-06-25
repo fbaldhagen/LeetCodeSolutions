927485783d02e9bdd6aa8ded04f240062ced2af8
fbaldhagen <baldhagen@gmail.com>
2024-06-25 13:59:27 +0200

    Problem 1038

---
+++ LeetCodeSolutions/1001-1100/Problems_1031_1040.cs
@@ -0,0 +1,38 @@
+﻿using LeetCodeSolutions.Structures;
+
+namespace LeetCodeSolutions._1001_1100;
+
+public class Problems_1031_1040
+{
+    /// <summary>
+    /// Problem 1038
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public static TreeNode BstToGst(TreeNode root)
+    {
+        // Track the sum of the Tree
+        int sum = 0;
+
+        // Use a helper method to traverse the tree, add to the sum and modify the node values.
+        BstToGstHelper(root);
+
+        // Return the modified tree
+        return root;
+
+        // Helper method to traverse and modify values.
+        // Start with larger values, add to sum, update node value to current value + sum.
+        TreeNode? BstToGstHelper(TreeNode? root)
+        {
+            if (root is not null)
+            {
+                BstToGstHelper(root.right);
+                sum += root.val;
+                root.val = sum;
+                BstToGstHelper(root.left);
+            }
+
+            return root;
+        }
+    }
+}
