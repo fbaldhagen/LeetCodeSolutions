afd8308b9bcac17a59594ce4f8ec2fdff2cf2dd9
fbaldhagen <baldhagen@gmail.com>
2024-06-25 15:18:06 +0200

    Problem 349

--- LeetCodeSolutions/0301-0400/Problems_0341_0350.cs
+++ LeetCodeSolutions/0301-0400/Problems_0341_0350.cs
@@ -17,4 +17,27 @@ public class Problems_0341_0350
             i++;
         }
     }
+
+    /// <summary>
+    /// Problem 349
+    /// </summary>
+    /// <param name="nums1"></param>
+    /// <param name="nums2"></param>
+    /// <returns></returns>
+    public static int[] Intersection(int[] nums1, int[] nums2)
+    {
+        HashSet<int> set1 = new(nums1);
+
+        HashSet<int> intersection = [];
+
+        for (int i = 0; i < nums2.Length; i++)
+        {
+            if (set1.Contains(nums2[i]))
+            {
+                intersection.Add(nums2[i]);
+            }
+        }
+
+        return [.. intersection];
+    }
 }
