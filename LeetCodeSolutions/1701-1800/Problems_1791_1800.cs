5fa311acb13d849a4703a822feebbeaf050301b9
fbaldhagen <baldhagen@gmail.com>
2024-06-27 13:49:30 +0200

    Problerm 1791

---
+++ LeetCodeSolutions/1701-1800/Problems_1791_1800.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeSolutions._1701_1800;
+
+public class Problems_1791_1800
+{
+    /// <summary>
+    /// Problems 1791
+    /// </summary>
+    /// <param name="edges"></param>
+    /// <returns></returns>
+    public static int FindCenter(int[][] edges)
+    {
+        int node1 = edges[0][0];
+        int node2 = edges[0][1];
+        int node3 = edges[1][0];
+        int node4 = edges[1][1];
+
+        if (node1 == node3 || node1 == node4)
+        {
+            return node1;
+        }
+        else
+        {
+            return node2;
+        }
+    }
+}
