11986dfead924951c93cbb807f1a342a786411a8
fbaldhagen <baldhagen@gmail.com>
2024-08-14 12:46:33 +0200

    Problem 719

---
+++ LeetCodeSolutions/0701-0800/Problems_0711_0720.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions._0701_0800
+{
+    public class Problems_0711_0720
+    {
+        /// <summary>
+        /// Problem 719
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int SmallestDistancePair(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int n = nums.Length;
+            int low = 0;
+            int high = nums[n - 1] - nums[0];
+
+            int CountPairsWithDistanceLessThanOrEqual(int mid)
+            {
+                int count = 0;
+                int left = 0;
+
+                for (int right = 0; right < n; right++)
+                {
+                    while (nums[right] - nums[left] > mid)
+                    {
+                        left++;
+                    }
+
+                    count += right - left;
+                }
+
+                return count;
+            }
+
+            while (low < high)
+            {
+                int mid = (low + high) / 2;
+
+                if (CountPairsWithDistanceLessThanOrEqual(mid) < k)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+
+            return low;
+        }
+    }
+}
