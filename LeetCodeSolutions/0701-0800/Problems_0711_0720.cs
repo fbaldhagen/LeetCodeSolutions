567344a0e7087617ba4656a55bf89d477fb53b1a
fbaldhagen <baldhagen@gmail.com>
2024-08-15 22:07:23 +0200

    Problem 719

--- LeetCodeSolutions/0701-0800/Problems_0711_0720.cs
+++ LeetCodeSolutions/0701-0800/Problems_0711_0720.cs
@@ -1,59 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LeetCodeSolutions._0701_0800;
 
-namespace LeetCodeSolutions._0701_0800
+public class Problems_0711_0720
 {
-    public class Problems_0711_0720
+    /// <summary>
+    /// Problem 719
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public static int SmallestDistancePair(int[] nums, int k)
     {
-        /// <summary>
-        /// Problem 719
-        /// </summary>
-        /// <param name="nums"></param>
-        /// <param name="k"></param>
-        /// <returns></returns>
-        public static int SmallestDistancePair(int[] nums, int k)
+        Array.Sort(nums);
+        int n = nums.Length;
+        int low = 0;
+        int high = nums[n - 1] - nums[0];
+
+        int CountPairsWithDistanceLessThanOrEqual(int mid)
         {
-            Array.Sort(nums);
-            int n = nums.Length;
-            int low = 0;
-            int high = nums[n - 1] - nums[0];
+            int count = 0;
+            int left = 0;
 
-            int CountPairsWithDistanceLessThanOrEqual(int mid)
+            for (int right = 0; right < n; right++)
             {
-                int count = 0;
-                int left = 0;
-
-                for (int right = 0; right < n; right++)
+                while (nums[right] - nums[left] > mid)
                 {
-                    while (nums[right] - nums[left] > mid)
-                    {
-                        left++;
-                    }
-
-                    count += right - left;
+                    left++;
                 }
 
-                return count;
+                count += right - left;
             }
 
-            while (low < high)
-            {
-                int mid = (low + high) / 2;
-
-                if (CountPairsWithDistanceLessThanOrEqual(mid) < k)
-                {
-                    low = mid + 1;
-                }
-                else
-                {
-                    high = mid;
-                }
-            }
-
-            return low;
+            return count;
         }
+
+        while (low < high)
+        {
+            int mid = (low + high) / 2;
+
+            if (CountPairsWithDistanceLessThanOrEqual(mid) < k)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+
+        return low;
     }
 }
