dc07a198f16aecf8bb204b9a95fc9b822a81558a
fbaldhagen <baldhagen@gmail.com>
2024-08-19 01:21:13 +0200

    Problem 372

--- LeetCodeSolutions/0301-0400/Problems_0371_0380.cs
+++ LeetCodeSolutions/0301-0400/Problems_0371_0380.cs
@@ -26,4 +26,49 @@ public class Problems_0371_0380
         // The sum is now stored in 'a'
         return a;
     }
+
+    /// <summary>
+    /// Problem 372
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static int SuperPow(int a, int[] b)
+    {
+        const int MOD = 1337;
+        return SuperPowHelper(a % MOD, b, b.Length - 1);
+
+        static int SuperPowHelper(int a, int[] b, int index)
+        {
+            if (index == -1)
+            {
+                return 1;
+            }
+
+            // Calculate (a^b[index]) % MOD
+            int part1 = PowMod(a, b[index]);
+
+            // Calculate (previous result^10) % MOD
+            int part2 = PowMod(SuperPowHelper(a, b, index - 1), 10);
+
+            // Multiply both parts and return the result modulo MOD
+            return (part1 * part2) % MOD;
+
+            static int PowMod(int x, int n)
+            {
+                int result = 1;
+                x %= MOD;
+                while (n > 0)
+                {
+                    if (n % 2 == 1)
+                    {
+                        result = (result * x) % MOD;
+                    }
+                    x = (x * x) % MOD;
+                    n /= 2;
+                }
+                return result;
+            }
+        }
+    }
 }
