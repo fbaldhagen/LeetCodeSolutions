3e685a333dfcc49d6a2b0c929b7def512aa4365b
fbaldhagen <baldhagen@gmail.com>
2024-09-01 23:55:33 +0200

    Problem 2022

---
+++ LeetCodeSolutions/2001-2100/Problems_2021_2030.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions._2001_2100;
+
+public class Problems_2021_2030
+{
+    /// <summary>
+    /// Problem 2022
+    /// </summary>
+    /// <param name="original"></param>
+    /// <param name="m"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static int[][] Construct2DArray(int[] original, int m, int n)
+    {
+        // Step 1: Check if the total number of elements matches the desired dimensions
+        if (original.Length != m * n)
+        {
+            return [];
+        }
+
+        // Step 2: Initialize the 2D array
+        int[][] result = new int[m][];
+        for (int i = 0; i < m; i++)
+        {
+            result[i] = new int[n];
+        }
+
+        // Step 3: Fill the 2D array with elements from the 1D array
+        int index = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                result[i][j] = original[index++];
+            }
+        }
+
+        return result;
+    }
+}
