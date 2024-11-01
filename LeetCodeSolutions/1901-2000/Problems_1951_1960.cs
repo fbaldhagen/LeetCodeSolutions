cf3d8b81ea2d58cf809635f5fbc89fce3567381a
fbaldhagen <baldhagen@gmail.com>
2024-11-01 15:26:31 +0100

    Problem 1957

---
+++ LeetCodeSolutions/1901-2000/Problems_1951_1960.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace LeetCodeSolutions._1901_2000;
+
+public class Problems_1951_1960
+{
+    /// <summary>
+    /// Problem 1957
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static string MakeFancyString(string s)
+    {
+        StringBuilder sb = new();
+
+        int cons = 1;
+        char prev = s[0];
+        sb.Append(prev);
+
+        for (int c = 1; c < s.Length; c++)
+        {
+            if (s[c].Equals(prev))
+            {
+                cons++;
+                if (cons != 3)
+                {
+                    sb.Append(s[c]);
+                }
+            }
+            else
+            {
+                sb.Append(s[c]);
+                cons = 1;
+                prev = s[c];
+            }
+        }
+
+        return sb.ToString();
+    }
+}
