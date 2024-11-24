8df6c873adb9c94d9a9a3bf0c82d363cc2a345b4
fbaldhagen <baldhagen@gmail.com>
2024-11-24 13:38:31 +0100

    Problem 1975

---
+++ LeetCodeSolutions/1901-2000/Problems_1971_1980.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeSolutions._1901_2000;
+
+public class Problems_1971_1980
+{
+    /// <summary>
+    /// Problem 1975
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <returns></returns>
+    public static long MaxMatrixSum(int[][] matrix)
+    {
+        int n = matrix.Length;
+        long sum = 0;
+        int minAbs = int.MaxValue;
+        int negativeCount = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                sum += Math.Abs(matrix[i][j]);
+                if (matrix[i][j] < 0)
+                {
+                    negativeCount++;
+                }
+                minAbs = Math.Min(minAbs, Math.Abs(matrix[i][j]));
+            }
+        }
+
+        if (negativeCount % 2 != 0)
+        {
+            sum -= 2 * minAbs;
+        }
+
+        return sum;
+    }
+}
