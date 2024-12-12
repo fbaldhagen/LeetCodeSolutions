795229aaf170184fb1a593881a44b50e6312427d
fbaldhagen <baldhagen@gmail.com>
2024-12-12 14:28:52 +0100

    Problem 2558

---
+++ LeetCodeSolutions/2501-2600/Problems_2551_2560.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeSolutions._2501_2600;
+
+public class Problems_2551_2560
+{
+    /// <summary>
+    /// Problem 2558
+    /// </summary>
+    /// <param name="gifts"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public static long PickGifts(int[] gifts, int k)
+    {
+        PriorityQueue<int, int> pq = new();
+        foreach (int gift in gifts)
+        {
+            pq.Enqueue(gift, -gift);
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            int maxGift = pq.Dequeue();
+            int leftBehind = (int)Math.Sqrt(maxGift);
+            pq.Enqueue(leftBehind, -leftBehind);
+        }
+
+        long sumLeft = 0;
+        while (pq.Count > 0)
+        {
+            sumLeft += pq.Dequeue();
+        }
+
+        return sumLeft;
+    }
+}
