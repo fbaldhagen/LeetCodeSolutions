e3f4d9f2ae3435aa1e4635ed6eb5e6b30591d39f
fbaldhagen <baldhagen@gmail.com>
2025-01-03 15:56:56 +0100

    Problem 2270

---
+++ LeetCodeSolutions/2201-2300/Problems_2261_2270.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCodeSolutions._2201_2300;
+
+public class Problems_2261_2270
+{
+    /// <summary>
+    /// Problem 2270
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public static int WaysToSplitArray(int[] nums)
+    {
+        int n = nums.Length;
+        int validSplits = 0;
+
+        long totalSum = 0;
+        foreach (int num in nums)
+        {
+            totalSum += num;
+        }
+
+        long leftSum = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            leftSum += nums[i];
+            long rightSum = totalSum - leftSum;
+
+            if (leftSum >= rightSum)
+            {
+                validSplits++;
+            }
+        }
+
+        return validSplits;
+    }
+}
